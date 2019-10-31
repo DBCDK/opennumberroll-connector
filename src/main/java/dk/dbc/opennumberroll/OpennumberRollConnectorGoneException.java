2903f728f3d4f293e8537d4853e664c289356f71
Henrik Witt-Hansen <hwha@dbc.dk>
2019-10-31 15:23:55 +0100

    Remove unused exception type
    Auditors: jbn, atm, mib, jbr

--- src/main/java/dk/dbc/opennumberroll/OpennumberRollConnectorGoneException.java
+++
@@ -1,9 +0,0 @@
-package dk.dbc.opennumberroll;
-
-public class OpennumberRollConnectorGoneException extends OpennumberRollConnectorUnexpectedStatusCodeException {
-
-    public OpennumberRollConnectorGoneException(String message) {
-        super(message, 410);
-    }
-
-}
